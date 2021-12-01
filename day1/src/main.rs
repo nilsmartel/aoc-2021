2b93f02c9b0ca78923c36f96317cad0a6ae7fe70
Nils Martel <nilsmartel@yahoo.de>
2021-12-01 10:57:57 +0100

    Upload solution to day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let lines = read_lines("../input/day1.txt")?;
+    let lines: Vec<i64> = lines.map(|l| l.unwrap().parse::<i64>().unwrap()).collect();
+
+    let count = lines.iter().zip(lines[1..].iter()).filter(|(a, b)| a < b).count();
+
+    println!("{}", count);
+
+    Ok(())
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
